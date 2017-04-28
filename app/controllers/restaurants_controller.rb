652e292407ec19f6e2da04340dcfb7e3ed90ed56
michael-palad <happyfighter2015@gmail.com>
2017-04-28 15:45:22 +0000

    add authentication to pages and edit/delete links

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_params, only: [:show, :edit, :update, :destroy]
   
   def index
