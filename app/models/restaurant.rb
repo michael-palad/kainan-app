57501961438f566474160e13a443b16b94106fe4
michael-palad <happyfighter2015@gmail.com>
2017-04-28 14:24:40 +0000

    add telephone number format validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,5 +8,22 @@ class Restaurant < ApplicationRecord
   validates :cuisine, 
             inclusion: { in: %w(Filipino Chinese American Korean Fastfood),
             message: "%{value} is not a valid cuisine category" }
+            
+  validate :check_telephone_number_format
+  
+  def check_telephone_number_format
+    valid_phone_formats = [
+      /^\+\d\d \d \d\d\d-\d\d\d\d$/,               # +63 2 123-4567
+      /^\d\d \d\d\d-\d\d\d\d$/,                    # 02 123-4567
+      /^\+\d\d \d \d\d\d-\d\d\d\d ext. \d\d\d$/    # +63 2 123-4567 ext. 103
+    ]
+                  
+    matched = valid_phone_formats.map do |re|
+      re.match(telephone_number)
+    end.any?
+    unless matched
+      errors.add(:telephone_number, "Invalid format for telephone number")
+    end
+  end
   
 end
