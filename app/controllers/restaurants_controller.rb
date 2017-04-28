f875103132b1e53bfcf01412dd9c3ea8c8896a17
michael-palad <happyfighter2015@gmail.com>
2017-04-28 13:12:14 +0000

    add create action and user_id to restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,5 +5,20 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+  
+  def create
+    @restaurant = current_user.restaurants.build(restaurant_params)
+    if @restaurant.save
+      redirect_to root_path  
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :telephone_number, :cuisine)  
+    end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428125605) do
+ActiveRecord::Schema.define(version: 20170428130845) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170428125605) do
     t.string   "cuisine"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
