33689d71a155df0a1894ab2518ab7dbd901eac04
michael-palad <happyfighter2015@gmail.com>
2017-04-28 14:03:27 +0000

    add validations for restaurant fields

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,11 @@ class Restaurant < ApplicationRecord
     
   # Associations
   belongs_to :user
+  
+  # Validation
+  validates_presence_of :name, :address, :telephone_number, :cuisine
+  validates :cuisine, 
+            inclusion: { in: %w(Filipino Chinese American Korean Fastfood),
+            message: "%{value} is not a valid cuisine category" }
+  
 end
