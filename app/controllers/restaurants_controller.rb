3f746db8d93064bcf66be7238dc8fe238a6d0c7d
michael-palad <happyfighter2015@gmail.com>
2017-04-28 14:35:12 +0000

    add edit action and links

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :find_params, only: [:show, :edit, :update]
+  
   def index
     @restaurants = Restaurant.order(created_at: :desc)
   end
   
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
   
   def new
@@ -20,7 +21,18 @@ class RestaurantsController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    
+  end
+  
   private
+  
+    def find_params
+      @restaurant = Restaurant.find(params[:id])  
+    end
   
     def restaurant_params
       params.require(:restaurant).permit(:name, :address, :telephone_number, :cuisine)  
