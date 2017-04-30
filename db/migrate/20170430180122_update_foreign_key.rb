11628c41fafa2ebc22b1aadc552978704382c668
michael-palad <happyfighter2015@gmail.com>
2017-04-30 18:10:26 +0000

    updated migration file again

--- db/migrate/20170430180122_update_foreign_key.rb
+++ db/migrate/20170430180122_update_foreign_key.rb
@@ -1,5 +1,7 @@
 class UpdateForeignKey < ActiveRecord::Migration[5.0]
   def change
+    remove_foreign_key :restaurants, :users
+    
     add_foreign_key :restaurants, :users, on_delete: :cascade
   end
 end
